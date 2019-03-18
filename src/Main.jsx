e6a8a4a4364296db17ac5da2f6da4c2e9e459e6d
cloudheadboatman <adam.boatman@gmail.com>
2019-03-18 09:15:18 +0000

    My React boilerplate

---
+++ src/Main.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Main = () => {
+    return <div>My React Boiler Plate</div>;
+}
+
+export default Main;
---
+++ webpack.common.js
@@ -0,0 +1,25 @@
+module.exports = {
+  entry: './src/index.jsx',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  }
+};
